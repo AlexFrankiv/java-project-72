9310f013dc842d929c9a9e8dcd9fe0fa518a240b
franAlex <frankivs@mail.ru>
2026-07-09 17:09:20 +0500

    Complete project

--- app/src/main/java/hexlet/code/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/UrlsPage.java
@@ -1,19 +1,4 @@
 package hexlet.code.dto;
 
-import lombok.Getter;
-
-
-@Getter
-public class UrlsPage extends BasePage {
-    private String error;   // сообщение об ошибке
-    private String url;
-
-    public UrlsPage(String error, String url, String flash) {
-        super(flash);
-        this.error = error;
-        this.url = url;
-    }
-    public UrlsPage(String error, String url) {
-        this(error, url, null);
-    }
+public class UrlsPage {
 }
